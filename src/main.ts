46f6bee598a56c1211d15b007250fdadcf52317c
antonio-abrantes <antonio-abrantes@hotmail.com.br>
2026-04-23 20:19:58 -0300

    adjusted error in docker image build

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,16 @@
 import { NestFactory } from '@nestjs/core';
-import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
-    new FastifyAdapter()
+    new FastifyAdapter(),
   );
-  
+
   // Porta default para o desenvolvimento local
   await app.listen(3000, '0.0.0.0');
   console.log(`Softconnect API rodando em: http://127.0.0.1:3000`);
